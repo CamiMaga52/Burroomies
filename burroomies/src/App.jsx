f5e95e5cf2a66c2d75392a3a2e207a36619963b3
Naii02 <ncasvazq@gmail.com>
2026-03-17 17:13:56 -0600

    actualizar App.jsx con rutas

--- burroomies/src/App.jsx
+++ burroomies/src/App.jsx
@@ -1,11 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import InicioArrendador from "./arrendador/InicioArrendador";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/arrendador" element={
+          <InicioArrendador
+            onAgregarVivienda={() => {}}
+            onMisViviendas={() => {}}
+            onMisArrendamientos={() => {}}
+            onVerPerfil={() => {}}
+            onCerrarSesion={() => {}}
+          />
+        } />
       </Routes>
     </BrowserRouter>
   );
